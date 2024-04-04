4133222135c686aa97825d335204cad48afa3b5e
Marek Vajgl <marek.vajgl@osu.cz>
2024-04-04 23:49:54 +0200

    ... IN PROGRESS

--- Com2vPilotVolume/Types/AppSimCon.cs
+++ Com2vPilotVolume/Types/AppSimCon.cs
@@ -1,6 +1,7 @@
 ﻿using ESimConnect;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -59,6 +60,10 @@ namespace eng.com2vPilotVolume.Types
     private readonly ESimConnect.ESimConnect eSimCon;
     private readonly ELogging.Logger logger;
     private int comVolumeTypeId = INT_EMPTY;
+    private int comVolumeRequestId = INT_EMPTY;
+    private int com1ReceiveEventId = INT_EMPTY;
+    private int com2ReceiveEventId = INT_EMPTY;
+    private int com3ReceiveEventId = INT_EMPTY;
 
     #endregion Private Fields
 
@@ -107,9 +112,18 @@ namespace eng.com2vPilotVolume.Types
         this.eSimCon.Open();
 
         // on success:
-        RegisterToSimCon();
-        this.connectionTimer.Enabled = false;
-        this.logger.Log(ELogging.LogLevel.INFO, "Connection enabled.");
+        try
+        {
+          RegisterToSimCon();
+          this.connectionTimer.Enabled = false;
+          this.logger.Log(ELogging.LogLevel.INFO, "Connection enabled.");
+          this.State.IsConnected = true;
+        }
+        catch (Exception ex)
+        {
+          this.logger.Log(ELogging.LogLevel.ERROR, "Fatal error registering simcon variables: " + ex.Message);
+          throw new ApplicationException("Failed to register simcon definitions.", ex);
+        }
       }
       catch (Exception ex)
       {
@@ -120,7 +134,7 @@ namespace eng.com2vPilotVolume.Types
 
     private void ESimCon_DataReceived(ESimConnect.ESimConnect sender, ESimConnect.ESimConnect.ESimConnectDataReceivedEventArgs e)
     {
-      if (e.RequestId != this.comVolumeTypeId) return;
+      if (e.RequestId != this.comVolumeRequestId) return;
 
       double volumeDouble = (double)e.Data;
       Volume volume = volumeDouble;
@@ -130,11 +144,29 @@ namespace eng.com2vPilotVolume.Types
 
     private void RegisterToSimCon()
     {
-      int typeId = this.eSimCon.RegisterPrimitive<double>(SimVars.Aircraft.RadioAndNavigation.COM_VOLUME);
-      this.eSimCon.RequestDataRepeatedly<double>(typeId, Microsoft.FlightSimulator.SimConnect.SIMCONNECT_PERIOD.SIM_FRAME, true);
       this.eSimCon.DataReceived += ESimCon_DataReceived;
+      int typeId = this.eSimCon.RegisterPrimitive<double>(SimVars.Aircraft.RadioAndNavigation.COM_VOLUME);
+      this.eSimCon.RequestPrimitiveRepeatedly(typeId, out int requestId, Microsoft.FlightSimulator.SimConnect.SIMCONNECT_PERIOD.SIM_FRAME, true);
       this.comVolumeTypeId = typeId;
+      this.comVolumeRequestId = requestId;
+
+      this.eSimCon.EventInvoked += ESimCon_EventInvoked;
+      this.com1ReceiveEventId = this.eSimCon.RegisterSystemEvent("COM1_RECEIVE_SELECT");
+      this.com2ReceiveEventId = this.eSimCon.RegisterSystemEvent("COM2_RECEIVE_SELECT");
+      this.com3ReceiveEventId = this.eSimCon.RegisterSystemEvent("COM3_RECEIVE_SELECT");
+
     }
+
+    private void ESimCon_EventInvoked(ESimConnect.ESimConnect sender, ESimConnect.ESimConnect.ESimConnectEventInvokedEventArgs e)
+    {
+      if (e.RequestId == this.com1ReceiveEventId)
+        this.State.ActiveComIndex = 1;
+      else if (e.RequestId == this.com2ReceiveEventId)
+        this.State.ActiveComIndex = 2;
+      else if (e.RequestId == this.com3ReceiveEventId)
+        this.State.ActiveComIndex = 3;
+    }
+
     private void StartIfNotConnected()
     {
       if (connectionTimer.Enabled) return;
