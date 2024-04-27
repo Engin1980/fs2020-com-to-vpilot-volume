6e5af64a2af126877acde73924b90cb208c5e6d0
Marek Vajgl <engin@seznam.cz>
2024-04-27 22:08:59 +0200

    Add DLL update via BAT from ESimConnect

--- _updateDLLs.bat
+++ _updateDLLs.bat
@@ -3,7 +3,11 @@ set "local=%cd%"
 cd ..\ESystem.NET
 call .\_updateRelease.bat
 
+cd ..\ESimConnect
+call .\_updateRelease.bat
+
 cd %local%
 copy ..\ESystem.NET\_Release\* .\DLLs\
+copy ..\ESimConnect\_Release\* .\DLLs\
 
 pause
